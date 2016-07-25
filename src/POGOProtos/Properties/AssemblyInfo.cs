a45cd4e5c064c960a17746aa9896579194e8eda1
John Du Hart <john@johnduhart.me>
2016-07-24 21:56:32 -0400

    Lowering to .NETStandard 1.0

--- src/POGOProtos/Properties/AssemblyInfo.cs
+++ src/POGOProtos/Properties/AssemblyInfo.cs
@@ -14,6 +14,3 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2e02c14d-68a5-4381-8ac2-d18f834f5c2d")]
